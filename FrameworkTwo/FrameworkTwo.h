2491e916bd6061ea7225b449d8625cda3547cfab
muruganandam <muruganandam@PSSHSRLT020.local>
2018-06-26 12:50:54 +0530

    Initial commit

---
+++ FrameworkTwo/FrameworkTwo.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTwo.h
+//  FrameworkTwo
+//
+//  Created by muruganandam on 26/06/18.
+//  Copyright © 2018 com.prematix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkTwo.
+FOUNDATION_EXPORT double FrameworkTwoVersionNumber;
+
+//! Project version string for FrameworkTwo.
+FOUNDATION_EXPORT const unsigned char FrameworkTwoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTwo/PublicHeader.h>
+
+
---
+++ FrameworkTwo/secondfw.swift
@@ -0,0 +1,16 @@
+//
+//  secondfw.swift
+//  secondframework
+//
+//  Created by muruganandam on 25/06/18.
+//  Copyright © 2018 com.prematix. All rights reserved.
+//
+
+import Foundation
+public class secondfw
+{
+    public init() {}
+   open func testfunction() {
+        print("second framework is running!!")
+    }
+}
